1f9f303669c42479dc10b4b56889853011d3badd
Dondoco <mail@dndc.dev>
2024-08-07 09:06:44 +0900

    Launcherは一旦削除

--- gyazo.js
+++ gyazo.js
@@ -1,12 +1,11 @@
-function get_data() {
+function getData() {
     const current_url = window.location.href;
     const gyazo_url_regex = /https:\/\/gyazo.com\/[0-9a-fA-F]{32}/;
-    const gyazo_id_regex = /[0-9a-fA-F]{32}/;
 
     if (current_url.match(gyazo_url_regex)) {
-        console.log("Gyazoページです")
+        console.log("Gyazoページです");
+        chrome.runtime.sendMessage({ action: "addContextMenu" });
     }
-
 }
 
-get_data();
+getData();
